b1e097957c63e98cf4a6fcf2b1163e46669e4db9
itc-n24006 <n24006@std.it-college.ac.jp>
2024-08-07 06:37:34 +0000

    kadai

---
+++ CHAPTER06/Q6_4_1.py
@@ -0,0 +1,11 @@
+import random
+
+random.seed(1)
+msgs = ["Hi", "Hello", "Good morning", "Good night", "See you later", "How are you", "Have a good day"]
+with open("some.txt", "w") as f:
+    for i in range(1000000):
+        f.write("{}, {}\n".format(i, random.choice(msgs)))
+
+with open('some.txt') as f:
+    x = f.readlines()
+    print(''.join(x[:3]))
---
+++ CHAPTER06/Q6_4_2.py
@@ -0,0 +1,11 @@
+def a(x=2):
+    while True:
+        for i in range(a, x):
+            if x % i == 0:
+                break
+        else:
+            yieldx
+        x += 1
+
+
+
